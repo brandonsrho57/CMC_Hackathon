6b46d842a3ffe3f5cbd34e08f59055ce15eed149
brandonsrho57 <70169482+brandonsrho57@users.noreply.github.com>
2021-04-16 14:18:12 -0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,5 +1,5 @@
-username = 'USERNAME'
-password = 'PASSWORD'
+username = 'INSERT USERNAME HERE'
+password = 'INSERT PW HERE'
 account = 'kia11090.us-east-1'
 warehouse = 'XF_HACKATHON_WH'
 database = 'MI_XPRESSCLOUD'
