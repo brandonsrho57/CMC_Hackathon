9b63a62ed3dd065e027d2a788497c89a020e6563
Edward Wu <ewu23@students.claremontmckenna.edu>
2021-04-15 22:06:33 -0700

    added visualizations for social, environmental, and governance

--- spgscorevalue.py
+++ spgscorevalue.py
@@ -55,10 +55,6 @@ SELECT * FROM "MI_XPRESSCLOUD"."XPRESSFEED"."SPGSCORETYPE";
 sql6 = """
 SELECT * FROM "MI_XPRESSCLOUD"."XPRESSFEED"."SPGSCOREWEIGHT";
 """
-#data from CIQCOMPANYCROSSREF_SNL
-sql7 = """
-SELECT * FROM "MI_XPRESSCLOUD"."XPRESSFEED"."CIQCOMPANYCROSSREF_SNL";
-"""
 #cleaning spgscorevalue dataset
 df1 = pd.read_sql(sql1, conn)
 df1 = df1.dropna()
@@ -84,12 +80,6 @@ df6 = pd.read_sql(sql6, conn)
 df6 = df6.dropna()
 df6 = df6.drop_duplicates()
 #print(df6.head())
-#cleaning companycrossref_snl dataset
-df7 = pd.read_sql(sql7, conn)
-df7 = df7.rename(columns={'IDENTIFIERID':"INSTITUTIONID"})
-df7= df7.dropna()
-df7 = df7.drop_duplicates()
-print(df7.head())
 #merging datasets 1 and 2
 df12 = pd.merge(df1,df2,how='left', on='SAMINDUSTRYID')
 #print(df12)
@@ -103,11 +93,7 @@ df1234 = pd.merge(df123,df4,how='left',on='ASSESSMENTTYPEID')
 df12345 = pd.merge(df1234,df5,how='left',on='SCORETYPEID')
 #df12345 = df12345[['SCORETYPEID','SCORETYPENAME','SAMINDUSTRYNAME','SAMINDUSTRYID','SCOREVALUE']]
 #merging datasets 1,2,3,4,5, and 6
-df123456 = pd.merge(df12345,df6,how='left',on=['ASPECTID','SAMINDUSTRYID'])
-#merging datasets 1,2,3,4,5,6, and 7
-df1234567 = pd.merge(df123456,df7,how='left',on='INSTITUTIONID')
-df1234567 = df1234567[['INSTITUTIONID','SCORETYPENAME','SAMINDUSTRYNAME','SAMINDUSTRYID','SCOREVALUE']]
-print(df1234567.head())
+df = pd.merge(df12345,df6,how='left',on=['ASPECTID','SAMINDUSTRYID'])
 # df = df[['ASPECTID','ASPECTNAME']]
 # df = df.loc[(df['ASPECTID']==107) | (df['ASPECTID']==108) | (df['ASPECTID']==114) | (df['ASPECTID']==124)]
 # df = df.sort_values(by='ASPECTID').drop_duplicates()
@@ -232,7 +218,7 @@ for rect, label in zip(rects, labels):
     ax.text(rect.get_x() + rect.get_width() / 2, height+0.01, int(height),
             ha='center', va='bottom', fontsize=8)
 plt.show()
-#Time Series for Average Mean Scores
+#Time Series for Average ESG Mean Scores
 dftimeesg = df.loc[(df['ASPECTID']==107)]
 dftimeesg.dropna(subset=['ASPECTNAME'])
 dftimeesg.drop_duplicates()
@@ -242,9 +228,195 @@ fig, ax = plt.subplots(1,1)
 plt.plot(dftimeesg['ASSESSMENTYEAR'], dftimeesg['SCOREVALUE'],'bo-')
 plt.ylim([10, 50])
 ax.set(xlabel="Assessment Year",
-       ylabel="Average Score Value",
-       title="Average Score Value for Each Assessment Year")
+       ylabel="Average ESG Score Value",
+       title="Average ESG Score Value for Each Assessment Year")
 for x,y in zip(dftimeesg['ASSESSMENTYEAR'],dftimeesg['SCOREVALUE']):
+
+    label = "{:.2f}".format(y)
+
+    plt.annotate(label, # this is the text
+                 (x,y), # this is the point to label
+                 textcoords="offset points", # how to position the text
+                 xytext=(0,12), # distance from text to points (x,y)
+                 ha='center') # horizontal alignment can be left, right or center
+plt.show()
+#Counting the amount of companies for each year for Overall ESG Scores
+dftimeesgcount = df.loc[(df['ASPECTID']==107)]
+dftimeesgcount.dropna(subset=['ASPECTNAME'])
+dftimeesgcount.drop_duplicates()
+dftimeesgcount=dftimeesgcount.groupby(by='ASSESSMENTYEAR')['INSTITUTIONID'].count().reset_index()
+print(dftimeesgcount)
+#What does overall social scores look like?
+dfsoc = df.loc[(df['ASPECTID']==124)&(df["ASSESSMENTYEAR"]==2020)]
+dfsoc.dropna(subset=['ASPECTNAME'])
+dfsoc.drop_duplicates()
+x = dfsoc['SCOREVALUE']
+plt.style.use('ggplot')
+fig, ax=plt.subplots(1,1)
+n,bins,patches=ax.hist(x, bins =[0,10,20,30,40,50,60,70,80,90,100], color='orange')
+mean_soc=dfsoc['SCOREVALUE'].mean()
+# print(mean_soc)
+ax.axvline(mean_soc,color='black',label='Mean Social SCORE')
+ax.legend(loc=0)
+patches[0].set_fc("red")
+patches[1].set_fc("yellow")
+patches[2].set_fc("green")
+patches[3].set_fc("blue")
+patches[4].set_fc("aqua")
+patches[5].set_fc("indigo")
+patches[6].set_fc("lavendar")
+patches[7].set_fc("salmon")
+patches[8].set_fc("grey")
+plt.xticks(bins)
+plt.xlabel('Social Scores', fontsize=15)
+plt.ylabel('Frequency', fontsize=15)
+plt.title('Overall Social Scores 2020', fontsize=15)
+# plot values on top of bars
+rects = ax.patches
+labels = ["label%d" % i for i in range(len(rects))]
+  
+for rect, label in zip(rects, labels):
+    height = rect.get_height()
+    ax.text(rect.get_x() + rect.get_width() / 2, height+0.01, int(height),
+            ha='center', va='bottom', fontsize=8)
+plt.show()
+#Summary Statistics for Social SCORES
+print(dfsoc['SCOREVALUE'].describe())
+#Time Series for Average Social Mean Scores
+dftimesoc = df.loc[(df['ASPECTID']==124)]
+dftimesoc.dropna(subset=['ASPECTNAME'])
+dftimesoc.drop_duplicates()
+dftimesoc = dftimesoc.groupby('ASSESSMENTYEAR')['SCOREVALUE'].mean().reset_index()
+print(dftimesoc)
+fig, ax = plt.subplots(1,1)
+plt.plot(dftimesoc['ASSESSMENTYEAR'], dftimesoc['SCOREVALUE'],'bo-')
+plt.ylim([10, 50])
+ax.set(xlabel="Assessment Year",
+       ylabel="Average Social Score Value",
+       title="Average Social Score Value for Each Assessment Year")
+for x,y in zip(dftimesoc['ASSESSMENTYEAR'],dftimesoc['SCOREVALUE']):
+
+    label = "{:.2f}".format(y)
+
+    plt.annotate(label, # this is the text
+                 (x,y), # this is the point to label
+                 textcoords="offset points", # how to position the text
+                 xytext=(0,12), # distance from text to points (x,y)
+                 ha='center') # horizontal alignment can be left, right or center
+plt.show()
+#Counting the amount of companies for each year for social
+dftimesoccount = df.loc[(df['ASPECTID']==124)]
+dftimesoccount.dropna(subset=['ASPECTNAME'])
+dftimesoccount.drop_duplicates()
+dftimesoccount=dftimesoccount.groupby(by='ASSESSMENTYEAR')['INSTITUTIONID'].count().reset_index()
+print(dftimesoccount)
+#What does overall environmental scores look like?
+dfenv = df.loc[(df['ASPECTID']==108)&(df["ASSESSMENTYEAR"]==2020)]
+dfenv.dropna(subset=['ASPECTNAME'])
+dfenv.drop_duplicates()
+x = dfenv['SCOREVALUE']
+plt.style.use('ggplot')
+fig, ax=plt.subplots(1,1)
+n,bins,patches=ax.hist(x, bins =[0,10,20,30,40,50,60,70,80,90,100], color='orange')
+mean_env=dfenv['SCOREVALUE'].mean()
+# print(mean_env)
+ax.axvline(mean_env,color='black',label='Mean Environmental Score')
+ax.legend(loc=0)
+patches[0].set_fc("red")
+patches[1].set_fc("yellow")
+patches[2].set_fc("green")
+patches[3].set_fc("blue")
+patches[4].set_fc("aqua")
+patches[5].set_fc("indigo")
+patches[6].set_fc("lavendar")
+patches[7].set_fc("salmon")
+patches[8].set_fc("grey")
+plt.xticks(bins)
+plt.xlabel('Environmental Scores', fontsize=15)
+plt.ylabel('Frequency', fontsize=15)
+plt.title('Environmental Social Scores 2020', fontsize=15)
+# plot values on top of bars
+rects = ax.patches
+labels = ["label%d" % i for i in range(len(rects))]
+  
+for rect, label in zip(rects, labels):
+    height = rect.get_height()
+    ax.text(rect.get_x() + rect.get_width() / 2, height+0.01, int(height),
+            ha='center', va='bottom', fontsize=8)
+plt.show()
+#Summary Statistics for Environmental SCORES
+print(dfenv['SCOREVALUE'].describe())
+#Time Series for Average Environmental Mean Scores
+dftimeenv = df.loc[(df['ASPECTID']==108)]
+dftimeenv.dropna(subset=['ASPECTNAME'])
+dftimeenv.drop_duplicates()
+dftimeenv = dftimeenv.groupby('ASSESSMENTYEAR')['SCOREVALUE'].mean().reset_index()
+#print(dftimeenv)
+fig, ax = plt.subplots(1,1)
+plt.plot(dftimeenv['ASSESSMENTYEAR'], dftimeenv['SCOREVALUE'],'bo-')
+plt.ylim([10, 50])
+ax.set(xlabel="Assessment Year",
+       ylabel="Average Environmental Score Value",
+       title="Average Environmental Score Value for Each Assessment Year")
+for x,y in zip(dftimeenv['ASSESSMENTYEAR'],dftimeenv['SCOREVALUE']):
+
+    label = "{:.2f}".format(y)
+
+    plt.annotate(label, # this is the text
+                 (x,y), # this is the point to label
+                 textcoords="offset points", # how to position the text
+                 xytext=(0,12), # distance from text to points (x,y)
+                 ha='center') # horizontal alignment can be left, right or center
+plt.show()
+#What does overall governance scores look like?
+dfgov = df.loc[(df['ASPECTID']==114)&(df["ASSESSMENTYEAR"]==2020)]
+dfgov.dropna(subset=['ASPECTNAME'])
+dfgov.drop_duplicates()
+x = dfgov['SCOREVALUE']
+plt.style.use('ggplot')
+fig, ax=plt.subplots(1,1)
+n,bins,patches=ax.hist(x, bins =[0,10,20,30,40,50,60,70,80,90,100], color='orange')
+mean_gov=dfgov['SCOREVALUE'].mean()
+# print(mean_gov)
+ax.axvline(mean_gov,color='black',label='Mean Governance SCORE')
+ax.legend(loc=0)
+patches[0].set_fc("red")
+patches[1].set_fc("yellow")
+patches[2].set_fc("green")
+patches[3].set_fc("blue")
+patches[4].set_fc("aqua")
+patches[5].set_fc("indigo")
+patches[6].set_fc("lavendar")
+patches[7].set_fc("salmon")
+patches[8].set_fc("grey")
+plt.xticks(bins)
+plt.xlabel('Governance Scores', fontsize=15)
+plt.ylabel('Frequency', fontsize=15)
+plt.title('Overall Governance Scores 2020', fontsize=15)
+# plot values on top of bars
+rects = ax.patches
+labels = ["label%d" % i for i in range(len(rects))]
+  
+for rect, label in zip(rects, labels):
+    height = rect.get_height()
+    ax.text(rect.get_x() + rect.get_width() / 2, height+0.01, int(height),
+            ha='center', va='bottom', fontsize=8)
+plt.show()
+#Summary Statistics for Governance SCORES
+print(dfgov['SCOREVALUE'].describe())
+#Time Series for Average Governance Mean Scores
+dftimegov = df.loc[(df['ASPECTID']==114)]
+dftimegov.dropna(subset=['ASPECTNAME'])
+dftimegov.drop_duplicates()
+dftimegov = dftimegov.groupby('ASSESSMENTYEAR')['SCOREVALUE'].mean().reset_index()
+#print(dftimeenv)
+fig, ax = plt.subplots(1,1)
+plt.plot(dftimegov['ASSESSMENTYEAR'], dftimegov['SCOREVALUE'],'bo-')
+plt.ylim([10, 60])
+ax.set(xlabel="Assessment Year",
+       ylabel="Average Governance Score Value",
+       title="Average Governance Score Value for Each Assessment Year")
+for x,y in zip(dftimegov['ASSESSMENTYEAR'],dftimegov['SCOREVALUE']):
 
     label = "{:.2f}".format(y)
 
